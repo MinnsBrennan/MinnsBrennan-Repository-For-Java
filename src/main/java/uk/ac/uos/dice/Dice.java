7df1b526fb1393500f825e5eb2f13d4d2c2a5f85
Brennan Minns <s195215@uos.ac.uk>
2018-10-21 17:26:58 +0100

    finished task 2

---
+++ src/main/java/uk/ac/uos/dice/Dice.java
@@ -0,0 +1,25 @@
+package uk.ac.uos.dice;
+
+import java.util.*;
+
+public class Dice {
+	
+	 public static void main(String[] args) {
+		 int dice1[] = {1, 2, 3, 4, 5, 6};
+		 int dice2[] = {1, 2, 3, 4, 5, 6};
+		 ArrayList<Integer> arrayList = DiceCalculator(dice1, dice2);
+		 System.out.println("All dice combinations: " + arrayList);
+	 }
+	 
+	public static ArrayList<Integer> DiceCalculator(int dice1[], int dice2[]) {
+		ArrayList<Integer> arrayList=new ArrayList<Integer>();
+		for(int x = 0; x < dice2.length; x++) {
+			for(int i = 0; i < dice1.length; i++) {
+				if(x < dice2.length) {
+					arrayList.add(dice1[i] + dice2[x]);
+				}
+			}
+		}
+		return arrayList;
+	}
+}
